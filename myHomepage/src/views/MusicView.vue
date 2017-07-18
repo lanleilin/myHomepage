9ce2307419cf6f7b3c2f25eea03e9bfe869b9242
lanleilin <2476081528@qq.com>
2017-07-18 19:43:59 +0800

    Update MusicView.vue

--- myHomepage/src/views/MusicView.vue
+++ myHomepage/src/views/MusicView.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="group-view has-header">
-		<p id="hello">hello</p>
+		<p id="hello" @click='fade'>hello</p>
+		<!--<p @click='fade'>world</p>-->
 		<!--video begins-->
 		<div class="wrap">
 			<video id="myVideo" width="350" height="250">
@@ -9,21 +10,25 @@
 			<!--<audio id="summervibe" src="./static/myMusic/summervibe.mp3" controls="controls"></audio>-->
 		</div>
 		<!--video ends-->
+		<div class="wrap">
 		<!--panel begins-->
-		<div class="m-panel">
-			<ul>
-				<li @click='changeVideo1'>sugar</li>
-				<li @click='changeVideo2'>我心的形状</li>
-				<li @click='changeVideo3'>加勒比海盗</li>
-				<li @click='changeVideo4'>main title</li>
-				<li @click='changeVideo5'>柔声倾述</li>
+		<div class="m-panel" id="m-panel">
+			<ul v-for="value in datalist">
+				<li>
+					<p class="videoLi"> {{value.name}}</p><span class="m-del"></span></li>
 			</ul>
+			<span class="panel-botton" @click="exitPanel" id="exitPanel">退出</span> 
+			<span class="panel-botton" @click="clearVideo" id="clearPanel">清空列表</span>
 		</div>
 		<!--panel ends-->
+		</div>
+		<div class="mask">
+
+		</div>
 		<!--control begins-->
 		<div class="m-control">
 			<ul>
-				<li>
+				<li @click="fade">
 					<img src="../assets/img/icon/icon-mode-loop.png" />
 				</li>
 				<li @click='prevVideo'>
@@ -35,7 +40,7 @@
 				<li @click='nextVideo'>
 					<img src="../assets/img/icon/icon-ctrl-right.png" />
 				</li>
-				<li>
+				<li @click="switchVideo">
 					<img src="../assets/img/icon/icon-song-list.png" />
 				</li>
 			</ul>
@@ -45,31 +50,53 @@
 </template>
 
 <script>
-	//	(function() {
-	//		var html = document.documentElement;
-	//		var hWidth = html.getBoundingClientRect().width;
-	//		html.style.fontSize = hWidth / 15 + "px"; //设置默认字符大小 25
-	//		var a = html.style.fontSize;
-	//	})();
-	let a = 1;
+	(function() {
+		var html = document.documentElement;
+		var hWidth = html.getBoundingClientRect().width;
+		//			html.style.fontSize = hWidth / 15 + "px"; //设置默认字符大小 25
+		html.style.fontSize = hWidth / 37.5 + "px";
+		var a = html.style.fontSize;
+	})();
+	let index = 1;
+	let ctrl = 1;
+	import animate from 'animate.css';
 
 	import {
 		mapState
 	} from 'vuex'
-
 	import UserBar from '../components/UserBar'
 	import Group from '../components/Group'
 	import DownloadApp from '../components/DownloadApp'
 
 	export default {
-		name: 'group-view',
+		name: 'music-view',
 		components: {
 			UserBar,
 			Group,
 			DownloadApp
 		},
 		data() {
-			return {}
+			return {
+				datalist: [{
+					name: 'sugar',
+					index: 1
+				}, {
+					name: '我心的形状',
+					index: 2
+				}, {
+					name: '加勒比海盗',
+					index: 3
+				}, {
+					name: 'main title',
+					index: 4
+				}, {
+					name: 'main theme',
+					index: 5
+				}],
+				videoName: '',
+				nowindex: -1
+			}
+
 		},
 		computed: {
 			// Getting Vuex State from store/modules/group
@@ -94,6 +121,7 @@
 				} else {
 					startIcon.src = '/static/icon/icon-ctrl-start.png'
 				}
+				myVideo.controls='';
 
 			},
 			nextVideo: function() {
@@ -102,84 +130,47 @@
 				let myVideo = document.getElementById('myVideo');
 				startIcon.src = '/static/icon/icon-ctrl-start.png';
 
-				a++;
-				if(a == 6) {
-					a = 1;
+				index++;
+				if(index == 6) {
+					index = 1;
 				}
-				console.log(a);
-				switch(a) {
-					case 2:
-						myVideo.src = '/static/myVideo/shapeOfMyHeart.mp4'
-						myVideo.play()
-						break;
-					case 3:
-						myVideo.src = '/static/myVideo/pirate.mp4'
-						myVideo.play()
-						break;
-					case 4:
-						myVideo.src = '/static/myVideo/mainTitle.mp4'
-						myVideo.play()
-						break;
-					case 5:
-						myVideo.src = '/static/myVideo/speakSoftlyLove.mp4'
-						myVideo.play()
-						break;
-					default:
-						myVideo.src = '/static/myVideo/sugar.mp4'
-						startIcon.src = '/static/icon/icon-ctrl-stop.png';
-				}
+				this.chooseVideo(index);
+
 			},
 			prevVideo: function() {
 				let startIcon = document.getElementById('start-icon');
 
 				let myVideo = document.getElementById('myVideo');
 				startIcon.src = '/static/icon/icon-ctrl-start.png';
-				a--;
-				if(a == 0) {
-					a = 5;
+				index--;
+				if(index == 0) {
+					index = 5;
 				}
-				console.log(a);
-				this.chooseVideo(a);
-				//				switch(a) {
-				//					case 2:
-				//						myVideo.src = '/static/myVideo/shapeOfMyHeart.mp4'
-				//						myVideo.play()
-				//						break;
-				//					case 3:
-				//						myVideo.src = '/static/myVideo/pirate.mp4'
-				//						myVideo.play()
-				//						this.sayTest();
-				//						break;
-				//					case 4:
-				//						myVideo.src = '/static/myVideo/mainTitle.mp4'
-				//						myVideo.play()
-				//						break;
-				//					case 5:
-				//						myVideo.src = '/static/myVideo/speakSoftlyLove.mp4'
-				//						myVideo.play()
-				//						break;
-				//					default:
-				//						myVideo.src = '/static/myVideo/sugar.mp4'
-				//						startIcon.src = '/static/icon/icon-ctrl-stop.png';
-				//				}
+				console.log(index);
+				this.chooseVideo(index);
+
 			},
-			chooseVideo: function(a) {
+			chooseVideo: function(index) {
 				let startIcon = document.getElementById('start-icon');
-				switch(a) {
+				switch(index) {
 					case 2:
 						myVideo.src = '/static/myVideo/shapeOfMyHeart.mp4'
+						startIcon.src = '/static/icon/icon-ctrl-start.png';
 						myVideo.play()
 						break;
 					case 3:
 						myVideo.src = '/static/myVideo/pirate.mp4'
+						startIcon.src = '/static/icon/icon-ctrl-start.png';
 						myVideo.play()
 						break;
 					case 4:
 						myVideo.src = '/static/myVideo/mainTitle.mp4'
+						startIcon.src = '/static/icon/icon-ctrl-start.png';
 						myVideo.play()
 						break;
 					case 5:
 						myVideo.src = '/static/myVideo/speakSoftlyLove.mp4'
+						startIcon.src = '/static/icon/icon-ctrl-start.png';
 						myVideo.play()
 						break;
 					default:
@@ -187,41 +178,60 @@
 						startIcon.src = '/static/icon/icon-ctrl-stop.png';
 				}
 			},
-			changeVideo2: function() {
-				let startIcon = document.getElementById('start-icon');
-				startIcon.src = '/static/icon/icon-ctrl-start.png';
-				a = 2;
-				myVideo.src = '/static/myVideo/shapeOfMyHeart.mp4'
-				myVideo.play()
-			},
-			changeVideo1: function() {
-				let startIcon = document.getElementById('start-icon');
-				startIcon.src = '/static/icon/icon-ctrl-start.png';
-				a = 1;
-				myVideo.src = '/static/myVideo/sugar.mp4'
-				myVideo.play()
-			},
-			changeVideo3: function() {
-				let startIcon = document.getElementById('start-icon');
-				startIcon.src = '/static/icon/icon-ctrl-start.png';
-				a = 3;
-				myVideo.src = '/static/myVideo/pirate.mp4'
-				myVideo.play()
-			},
-			changeVideo4: function() {
-				let startIcon = document.getElementById('start-icon');
-				startIcon.src = '/static/icon/icon-ctrl-start.png';
-				a = 4;
-				myVideo.src = '/static/myVideo/mainTitle.mp4'
 
-				myVideo.play()
-			},
-			changeVideo5: function() {
+			//switchViedeo 
+			switchVideo: function() {
+				//显示面板
+				let mPanel = document.getElementById('m-panel');
+				let mask = document.getElementsByClassName('mask')[0];
+				mask.style.display = 'block';
+				mask.className = "mask animated bounceInUp";
+//				mPanel.style.left=0+'px';
+				mPanel.style.display = 'block';
+				mPanel.className = "m-panel animated fadeIn"
+
 				let startIcon = document.getElementById('start-icon');
-				startIcon.src = '/static/icon/icon-ctrl-start.png';
-				a = 5;
-				myVideo.src = '/static/myVideo/speakSoftlyLove.mp4'
-				myVideo.play()
+				let videoLi = document.getElementsByClassName('videoLi');
+				let myVideo = document.getElementById('myVideo');
+				let mDel = document.getElementsByClassName('m-del');
+				let _this = this;
+				//				console.log(_this.datalist.length);
+				//切换视频 根据相应index播放相应视频
+				for(let i = 0; i < _this.datalist.length; i++) {
+					videoLi[i].addEventListener('click', function() {
+						let idx = _this.datalist[i].index;
+						_this.chooseVideo(idx);
+
+					})
+				}
+				//删除视频
+				for(let j = 0; j < mDel.length; j++) {
+					mDel[j].addEventListener('click', function() {
+						_this.datalist.splice(j, 1);
+						console.log(_this.datalist.length);
+					})
+				}
+
+			},
+			clearVideo: function() {
+				this.datalist = '';
+			},
+			exitPanel: function() {
+				let mPanel = document.getElementById('m-panel');
+//				mPanel.style.display='none'
+//				mPanel.style.left=-9999+'px';
+				mPanel.className = "m-panel animated zoomOut";
+				let mask = document.getElementsByClassName('mask')[0];
+//				mask.style.display = 'none';
+				mask.className='mask animated fadeOut'
+			},
+			fade: function() {
+//				alert('success');
+				let startIcon = document.getElementById('start-icon');
+				startIcon.src = '/static/icon/icon-ctrl-stop.png';
+				let myVideo = document.getElementById('myVideo');
+				myVideo.controls='controls';
+				myVideo.pause();
 			}
 
 		}
@@ -229,9 +239,14 @@
 </script>
 
 <style lang="scss" scoped>
+	* {
+		/*font-size: 62.5%;*/
+	}
+	
 	#hello {
 		color: #00B600;
 		/*color:#007722 ;*/
+		display: none;
 	}
 	
 	.list-link {
@@ -254,14 +269,73 @@
 	}
 	/*panel begins*/
 	
+	.m-panel {
+		display: none;
+		height: 20rem;
+		/*background: lightslategray;*/
+		position: absolute;
+		bottom: 7rem;
+		z-index: 1080;
+		width: 100%;
+		/*left: -9999px;*/
+	}
+	
 	.m-panel>ul>li {
 		width: 100%;
-		background: darkgray;
-		color: white;
+		/*background: lightcoral;*/
 		margin-top: 5px;
-		font-size: 32px;
+		font-size: 1.8rem;
+		padding-left: 1.2rem;
+		/*color: #403d3b;*/
+		color: white;
+		width: 25.4rem;
+		height: 3rem;
+		line-height: 3rem;
+		border-bottom: 1px solid #ededed;
+	}
+	.videoLi {
+		display: inline-block;
+		margin: 0;
+		padding: 0;
+	}
+	.panel-botton{
+		float: right;
+		position: absolute;
+		bottom: 0px;
+		color: white;
+		margin: 5px;
+	}
+	#clearPanel{
+		right: 20px;
+	}
+	#exitPanel{
+		right: 100px;
+	}
+	.m-del {
+		background: url(../assets/img/del.svg) center;
+		position: absolute;
+		right: 10px;
+		display: inline-block;
+		width: 3rem;
+		height: 3rem;
+		margin-right: 1.2rem;
+		line-height: 3rem;
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
 	}
 	/*panel ends*/
+	/*mask*/
+	.mask {
+		width: 100%;
+		height: 100%;
+		background: rgba(21,21,22,0.4);
+		position: absolute;
+		top: 0;
+		z-index: 990;
+		/*opacity: .4 !important;*/
+		display: none;
+	}
+	/*mask ends*/
 	/*add style*/
 	
 	.m-control {
@@ -271,6 +345,7 @@
 		bottom: 0;
 		background: #17130f;
 		opacity: 0.8;
+		z-index: 1024;
 	}
 	
 	.m-control li {
