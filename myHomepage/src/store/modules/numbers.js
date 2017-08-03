b48b3ef2c03d7c71119119303e99cc9b19b0cc6d
lanleilin <2476081528@qq.com>
2017-08-03 15:32:06 +0800

    rating

--- myHomepage/src/store/modules/numbers.js
+++ myHomepage/src/store/modules/numbers.js
@@ -1,26 +1,32 @@
 const state = {
-  group_a: [],
-  group_b: [],
-  group_c: [],
-  testNumber:9009,
-  changeNumber:42
+	group_a: [],
+	group_b: [],
+	group_c: [],
+	testNumber: 9009,
+	changeNumber: 2
 }
 
 const mutations = {
-		addChangeNum(state){
-			state.changeNumber++
-		}
+	addChangeNum(state) {
+		//			state.changeNumber++
+	}
 }
 
 const actions = {
-    addChangeNum({ commit }) {
-    //dosomething
-    commit('addChangeNum')
-  }
+	addChangeNum({
+		commit
+	}) {
+		//dosomething
+		state.changeNumber+=0.5
+		if(state.changeNumber>5){
+			state.changeNumber=0
+		}
+			commit('addChangeNum')
+	}
 }
 
 export default {
-  state,
-  mutations,
-  actions
+	state,
+	mutations,
+	actions
 }
--- myHomepage/src/views/MarketView.vue
+++ myHomepage/src/views/MarketView.vue
@@ -1,8 +1,6 @@
 <template>
 	<div class="group-view has-header">
 		<div class="wrap">
-			<p>hello</p>
-
 			<!--crousal begin-->
 			<el-carousel :interval="4000" type="card" height="12rem">
 				<el-carousel-item v-for="item in 6" :key="item">
@@ -29,11 +27,39 @@
 
 			</div>
 			<!--img contains begins-->
-			<el-progress type="circle" :percentage='14' id="circle"></el-progress>
+			<div class="circleWrap" @click='say'>
+				<el-progress type="circle" :percentage='80' id="circle"></el-progress>
+			</div>
+
 			<!--<p>数字  ：{{nb}}</p>-->
-			<p>{{test}}</p>
-			<p @click='addChangeNum'>{{change}}</p>
-			<el-button type="primary" @click='addChangeNum'>hello</el-button>
+			<p>{{valueRating}}</p>
+			<div class="circleWrap">
+				<el-rate v-model="change" disabled show-text text-color="#ff9900" text-template="{value}"></el-rate>
+				<el-button @click='addChangeNum' type='success' :plain="true">+</el-button>
+
+			</div>
+			<!--<p @click='addChangeNum'>{{change}}</p>-->
+			<!--折叠面板-->
+			<el-collapse v-model="activeName" accordion>
+				<el-collapse-item title="一致性 Consistency" name="1">
+					<div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
+					<div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+				</el-collapse-item>
+				<el-collapse-item title="反馈 Feedback" name="2">
+					<div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+					<div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+				</el-collapse-item>
+				<el-collapse-item title="效率 Efficiency" name="3">
+					<div>简化流程：设计简洁直观的操作流程；</div>
+					<div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
+					<div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
+				</el-collapse-item>
+				<el-collapse-item title="可控 Controllability" name="4">
+					<div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
+					<div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+				</el-collapse-item>
+			</el-collapse>
+
 		</div>
 	</div>
 </template>
@@ -42,7 +68,6 @@
 	(function() {
 		var html = document.documentElement;
 		var hWidth = html.getBoundingClientRect().width;
-		//			html.style.fontSize = hWidth / 15 + "px"; //设置默认字符大小 25
 		html.style.fontSize = hWidth / 37.5 + "px";
 		var a = html.style.fontSize;
 	})();
@@ -71,7 +96,9 @@
 					name: 'macbook2',
 					price: '179'
 				}],
-				value: ''
+				value: '',
+				valueRating: 2,
+				activeName: '1'
 			}
 
 		},
@@ -81,8 +108,8 @@
 				group_a: state => state.group.group_a,
 				group_b: state => state.group.group_b,
 				group_c: state => state.group.group_c,
-				test:state =>state.numbers.testNumber,
-				change:state =>state.numbers.changeNumber
+				test: state => state.numbers.testNumber,
+				change: state => state.numbers.changeNumber
 			})
 		},
 		created: function() {
@@ -95,7 +122,13 @@
 		methods: {
 			...mapActions([
 				'addChangeNum'
-			])
+			]),
+			say: function() {
+				this.valueRating += 2;
+				if(this.valueRating > 5) {
+					this.valueRating = 0;
+				}
+			}
 		}
 	}
 </script>
@@ -111,6 +144,10 @@
 		padding: 0;
 	}
 	/*crousal*/
+	
+	.el-carousel {
+		margin-top: 5.4rem;
+	}
 	
 	.el-carousel__item h3 {
 		color: #475669;
@@ -187,5 +224,19 @@
 			color: #f05f50;
 			font-weight: bold;
 		}
+	}
+	/*circleWrap*/
+	
+	.circleWrap {
+		width: 100%;
+		margin: 0 auto;
+		text-align: center;
+	}
+	
+	.el-rate {
+		/*display: inline-block;*/
+	}
+	.el-collapse{
+		margin-top: 1rem;
 	}
 </style>
